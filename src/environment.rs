6a355e0eba5c8cc0992d440651ef8623ccec1a3f
Joshua Schappel <jmschappel12@gmail.com>
2020-05-18 14:25:32 -0400

    Cleaned up environment

--- src/environment.rs
+++ src/environment.rs
@@ -3,26 +3,21 @@ use crate::interpreter::Value;
 use crate::errors::RuntimeError;
 use crate::lexer::Token;
 
-#[derive(Debug, Clone, PartialEq)]
-pub enum EnvType {
-    Global,
-    Scoped(Box<Environment>)
-}
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct Environment {
-    enclosing: EnvType,
+    enclosing: Option<Box<Environment>>,
     values: HashMap<String, Value>
 }
 
 impl Environment {
     pub fn new() -> Environment {
-        Environment { values: HashMap::new(), enclosing: EnvType::Global }
+        Environment { values: HashMap::new(), enclosing: None }
     }
 
     pub fn new_lexical(&mut self) -> Environment {
         let outer = self.clone();
-        Environment { values: HashMap::new(), enclosing:  EnvType::Scoped(Box::new(outer)) }
+        Environment { values: HashMap::new(), enclosing:  Some(Box::new(outer)) }
     }
 
     pub fn define(&mut self, name: String, value: Option<Value>) {
@@ -37,9 +32,8 @@ impl Environment {
             Some(val) => Ok(val.clone()),
             None => {
                 match self.enclosing {
-                    EnvType::Scoped(ref mut env) => env.get(token), 
-                    EnvType::Global => Err(RuntimeError::string_error(&token, format!("Undefined Identifier: {}", token.lexeme)))
-
+                    Some(ref mut env) => env.get(token), 
+                    None => Err(RuntimeError::string_error(&token, format!("Undefined Identifier: {}", token.lexeme)))
                 }
             }
         }
@@ -51,7 +45,7 @@ impl Environment {
             self.values.insert(name, value);
             return Ok(())
         }
-        if let EnvType::Scoped(ref mut x) = self.enclosing {
+        if let Some(ref mut x) = self.enclosing {
             return x.assign(token, value);
         }
         Err(RuntimeError::string_error(&token, format!("Undefined identifier: {}", token.lexeme)))
@@ -60,8 +54,8 @@ impl Environment {
     // TODO:: Better memory management
     pub fn return_outer_scope(&mut self) -> Environment {
         match self.enclosing {
-            EnvType::Global => self.clone(),
-            EnvType::Scoped(ref env) => *env.clone(),
+            None => self.clone(),
+            Some(ref env) => *env.clone(),
         }
     }
 }
